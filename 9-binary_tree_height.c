e7742a93e907931b1865174f54cc0ce8b29a5bd2
afmorac <andresfmora.catro@gmail.com>
2025-04-11 14:10:18 -0400

    height tested

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Mide la altura de un árbol binario
+ * @tree: Puntero al nodo raíz
+ *
+ * Return: Altura, o 0 si es NULL o no tiene hijos
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t izq, der;
+
+if (tree == NULL || tree->left == NULL || tree->right == NULL)
+return (0);
+
+izq = binary_tree_height(tree->left);
+der = binary_tree_height(tree->right);
+
+return ((izq > der ? izq : der) +1);
+}
