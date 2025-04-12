9ff9453d7080b1aa8a1ab10efb5d3e10e2605377
afmorac <andresfmora.catro@gmail.com>
2025-04-12 12:19:05 -0400

    tecnisisimo 14v54

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,7 +15,7 @@ return (0);
 left = binary_tree_height(tree->left);
 right = binary_tree_height(tree->right);
 
-return ((left > right ? left : right) + 1);
+return ((left > right ? left : right) +1);
 }
 
 /**
