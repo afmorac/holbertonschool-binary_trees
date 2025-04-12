7f9ff3afcdb1f805e447ac5e8b19ddcbc2ac2cc8
afmorac <andresfmora.catro@gmail.com>
2025-04-12 12:21:52 -0400

    tecnisisimo 14v54

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,13 +9,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 size_t left = 0, right = 0;
 
-if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+if (tree == NULL)
 return (0);
 
 left = binary_tree_height(tree->left);
 right = binary_tree_height(tree->right);
 
-return ((left > right ? left : right) +1);
+return (1 + (left > right ? left : right));
 }
 
 /**
