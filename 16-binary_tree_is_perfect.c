676314422419512ea4c2c7ce90abe838fed27fbd
afmorac <andresfmora.catro@gmail.com>
2025-04-13 00:16:56 -0400

    arboler perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,12 +1,10 @@
 #include "binary_trees.h"
-#include <math.h>
 
 /**
- * binary_tree_height - Mide la altura de un árbol binario
- * @tree: Puntero al nodo raíz
- * Return: Altura del árbol
+ * binary_tree_height - Calcula la altura del árbol
+ * @tree: Nodo raíz
+ * Return: Altura
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 size_t left = 0, right = 0;
@@ -21,11 +19,10 @@ return (1 + (left > right ? left : right));
 }
 
 /**
- * binary_tree_size - Cuenta el número de nodos de un árbol binario
- * @tree: Puntero al nodo raíz
- * Return: Cantidad de nodos
+ * binary_tree_size - Cuenta los nodos del árbol
+ * @tree: Nodo raíz
+ * Return: Cantidad total de nodos
  */
-
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 if (tree == NULL)
@@ -35,24 +32,26 @@ return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
 
 /**
- * binary_tree_is_perfect - Verifica si un árbol binario es perfecto
+ * binary_tree_is_perfect - Verifica si un árbol es perfecto
  * @tree: Puntero al nodo raíz
- * Return: 1 si es perfecto, 0 si no
+ * Return: 1 si es perfecto, 0 en caso contrario
  */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-size_t height, nodes;
+size_t height, size, expected_nodes = 1;
 
 if (tree == NULL)
 return (0);
 
 height = binary_tree_height(tree);
-nodes = binary_tree_size(tree);
+size = binary_tree_size(tree);
 
-if (nodes == (size_t)pow(2, height + 1) - 1)
-return (1);
-else
-return (0);
+/* expected_nodes = 2^height - 1 */
+while (height > 0)
+{
+expected_nodes *= 2;
+height--;
 }
-
+expected_nodes -= 1;
+return (size == expected_nodes);
+}
