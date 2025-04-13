bc2cedb845d01f7e4a06fb8acd30ef5feaddf024
afmorac <andresfmora.catro@gmail.com>
2025-04-13 00:27:49 -0400

    tester

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,6 +1,22 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_sibling - Encuentra el hermano de un nodo
+ * @node: Puntero al nodo
+ * Return: Puntero al hermano o NULL si no tiene
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+if (node == NULL || node->parent == NULL)
+return (NULL);
+
+if (node->parent->left == node)
+return (node->parent->right);
+else
+return (node->parent->left);
+}
+
+/**
  * binary_tree_uncle - Encuentra el tío de un nodo
  * @node: Puntero al nodo
  * Return: Puntero al tío o NULL si no tiene
